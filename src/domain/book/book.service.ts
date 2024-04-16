611522ff9f1850f901c8b510a8211298ff4da528
Kane <khoa.vukhoa3004@hcmut.edu.vn>
2024-04-16 21:25:18 +0700

    add example

--- src/domain/book/book.service.ts
+++ src/domain/book/book.service.ts
@@ -6,5 +6,15 @@ import { BookGetListRequest } from './request/book-get-list.request';
 export class BookService {
     constructor(private prismaService: PrismaService) {}
 
-    async getList(query: BookGetListRequest) {}
+    async getList(query: BookGetListRequest) {
+        const books = await this.prismaService.book.findMany({
+            where: {
+
+            },
+            select: {
+                // example
+            }
+        });
+
+    }
 }
